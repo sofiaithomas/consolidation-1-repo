aaf55d124796e0d5020b8a08f9493b24fe1bb93a
Sofia Thomas <sofia.i.thomas@gmail.com>
2024-04-20 13:20:04 -0400

    Created functions to choose a secret word & to count the occurences of a guess in the word

--- consolidation_1.py
+++ consolidation_1.py
@@ -5,3 +5,12 @@ import random
 print("Hello Player! Welcome to the Word Guessing Game!")
 print("Let's get started: ")
 print("------------------------------------------------")
+
+# function to choose a secret word
+def choose_word():
+    secret_word_bank = ["glowworm", "pneumonia", "jukebox", "zodiac", "espionage", "rhubarb"]
+    return random.choice(secret_word_bank)
+
+# function to count the number of times a guess is in the word
+def count_guess_occurences(word, letter):
+    return word.count(letter)
