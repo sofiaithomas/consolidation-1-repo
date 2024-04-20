c14c1df94ca9739b6e6f618b2c52f99b0f809b65
Sofia Thomas <sofia.i.thomas@gmail.com>
2024-04-19 21:55:17 -0400

    Initial Commit: Imported random & creating greetings to player

---
+++ consolidation_1.py
@@ -0,0 +1,7 @@
+# Word Guessing Game Consolidation
+
+import random
+
+print("Hello Player! Welcome to the Word Guessing Game!")
+print("Let's get started: ")
+print("------------------------------------------------")
